53e1d3d1a44b5a5650acbb2513d0600aa2db2762
Bhoomika Krishnarajanagar Mohan <bhoomika@rita.cecs.pdx.edu>
2022-03-14 21:40:30 -0700

    Test for SLL instructions

---
+++ tests/sll1.s
@@ -0,0 +1,28 @@
+addi a2, a2,45
+addi a3, a3,42
+sll a4, a2, a3
+addi a5,a5, -11
+addi a6,a6, 25
+sll a4, a5, a6
+addi a2, a2, 44
+addi a3, a3, -61
+sll a4, a2, a3
+addi a5, a5, -18 
+addi a6, a6, -88
+sll a4, a5, a6
+sll zero, a2, a3
+addi a2, a2,45
+addi a3, a3,42
+sll a4, a3, a2
+addi a5,a5, -11
+addi a6,a6, 25
+sll a4, a6, a5
+addi a2, a2, 44
+addi a3, a3, -61
+sll a4, a3, a2
+addi a5, a5, -18
+addi a6, a6, -88
+sll a4, a6, a5
+sll zero, a3, a2
+jr ra
+
