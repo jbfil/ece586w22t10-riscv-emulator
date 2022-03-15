da70782d0e0418a0eda1e6cb15a9378067af18cf
Mansi Dhanrajani <mansi2@ruby.cecs.pdx.edu>
2022-03-15 04:01:49 -0700

    branch updated

--- tests/branch4.s
+++ tests/branch4.s
@@ -3,8 +3,8 @@ main:
     addi a1, zero, 3
     blt a0, a1, ls
     beq a0, a1, equal
-equal:
-    add a2, a0, a1 
 ls: 
     sub a2, a1, a0 
+equal:
+     add a2, a0, a1
 jr ra
--- tests/branch5.s
+++ tests/branch5.s
@@ -1,8 +1,8 @@
  main:
     addi a0, zero, 1
     addi a1, zero, 5
-    blt  a0, a1, sl
-    bltu a0, a1, less
+    blt  a0, a1, less
+    bltu a0, a1, sl
 sl:
     addi a2,zero, 1
 less:
