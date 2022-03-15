f614f064eb7c15b7ff293e288d31cba8017f882a
Sanchita Leekha <sleekha@ruby.cecs.pdx.edu>
2022-03-14 23:29:54 -0700

    Subtract

---
+++ tests/slti1.s
@@ -0,0 +1,5 @@
+addi a12, a10, 156
+slti a12, a10, 12
+slti a12, a10, -5
+slti zero, a12, a10
+slti a12, a12, 0
---
+++ tests/sub1.s
@@ -0,0 +1,7 @@
+addi a1,a1,22
+addi a2,a2,100
+addi a3,a3,-10
+addi a4,a4,-250
+sub s1,a1,a2
+sub s2,a3,a4
+sub s3,s2,a1
